08a706f7b876eecc1afdc4cf00e47fb8f24b9302
HernanJr0 <hernanjunior90@gmail.com>
2024-05-29 04:03:11 -0400

    usando .env

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const routes = require('./routes')
 app.use(express.json())
 
 const mongoose = require('mongoose')
-// mongoose.connect(process.env.MONGO_API_SECRET)
-mongoose.connect(
-  'mongodb+srv://admin:xEkT7E4dlk5asAYd@cluster0.thgtamc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-)
+mongoose.connect(process.env.MONGO_API_SECRET)
 
 app.use(routes)
 
