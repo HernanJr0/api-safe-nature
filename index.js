19ea9facbbbe6f7131583edb8b06a3c2be90c739
HernanJr0 <hernanjunior90@gmail.com>
2024-05-29 03:57:27 -0400

    teste bd

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const routes = require('./routes')
 app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGO_API_SECRET)
+// mongoose.connect(process.env.MONGO_API_SECRET)
+mongoose.connect(
+  'mongodb+srv://admin:xEkT7E4dlk5asAYd@cluster0.thgtamc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+)
 
 app.use(routes)
 
