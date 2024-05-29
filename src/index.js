a9660f37c6912833e513e36623aa0e77294078e6
HernanJr0 <hernanjunior90@gmail.com>
2024-05-29 03:40:05 -0400

    adição do módulo dotenv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const routes = require('./routes')
@@ -5,9 +7,7 @@ const routes = require('./routes')
 app.use(express.json())
 
 const mongoose = require('mongoose')
-mongoose.connect(
-  'mongodb+srv://admin:xEkT7E4dlk5asAYd@cluster0.thgtamc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-)
+mongoose.connect(process.env.MONGO_API_SECRET)
 
 app.use(routes)
 
