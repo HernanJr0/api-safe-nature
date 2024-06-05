98eb8af4bda1c7a4f11bcfa2aa417665fb5f5a09
HernanJr0 <hernanjunior90@gmail.com>
2024-06-05 18:37:07 -0400

    chore: Update MongoDB connection configuration

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
-const routes = require('./routes')
-
 const app = express()
-
-app.use(cors())
-
-mongoose.connect(process.env.MONGO_API_SECRET, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+const routes = require('./routes')
 
 app.use(express.json())
 
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_API_SECRET)
+
 app.use(routes)
 
-const PORT = process.env.PORT || 8000
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`)
-})
+app.listen(8000)
