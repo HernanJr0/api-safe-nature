d113637858af96fe94cbb6cb6e5a8397aa709022
HernanJr0 <hernanjunior90@gmail.com>
2024-06-05 18:10:39 -0400

    chore: Add CORS headers to allow cross-origin requests

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 
-app.use(express.json())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGO_API_SECRET)
